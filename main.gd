a0b10cdfccd7676162cf0bb85eaee7938a88c01f
Vadim Lemmer <lemmeronne@gmail.com>
2023-04-13 23:21:23 +0600

    feat: creation and placement of walls equidistant from the center

---
+++ main.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var wall = preload("res://World/Wall/wall.tscn")
+@export var countWall = 5
+
+
+func _ready():
+	wall_setting(countWall, Vector3.ZERO)
+
+func wall_setting(count, center):
+	var angle_step = 2.0 * PI / count
+	var radius = 10 / (2 * tan(PI / count))
+	var angle = 0
+	for i in range(0, count):
+		var direction = Vector3(sin(angle), 0, cos(angle))
+		var pos = center + direction * radius
+
+		var newWall = wall.instantiate()
+		newWall.position = pos
+		newWall.rotation.y = angle
+		$World.add_child(newWall)
+
+		angle += angle_step
