e83b806f4bc9c90e25359a024d70995e1fc28e74
Vadim Lemmer <lemmeronne@gmail.com>
2023-04-20 15:05:30 +0600

    feat: removing mobs out of borders

--- Bull/bull.gd
+++ Bull/bull.gd
@@ -1,6 +1,12 @@
 extends CharacterBody3D
 
 @export var speed = 10
+var border_x = 40
+var border_z = 40
+
+func _process(delta):
+	if (abs(position.x) > abs(border_x)) or (abs(position.z) > abs(border_z)):
+		queue_free()
 
 func _physics_process(_delta):
 	move_and_slide()
